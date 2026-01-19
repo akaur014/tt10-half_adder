f07cc3971722a683819c0fc036943b2c8006a304
akaur014 <ia0111@wayne.edu>
2026-01-18 20:30:01 -0500

    Rename module from tt_um_akaur014_nand to tt_um_akaur014_half_adder

--- src/project.v
+++ src/project.v
@@ -4,7 +4,7 @@
  */
 
 `default_nettype none
-module tt_um_akaur014_nand (
+module tt_um_akaur014_half_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
